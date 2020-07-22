52b4f3abcc98aa1fda47e5b7051befa91ad8b212
Gabriel Barreto <gabriel.barret015@hotmail.com>
2020-07-21 23:13:13 -0300

    error handling in examsController

--- src/controllers/ExamsController.ts
+++ src/controllers/ExamsController.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import { Exams } from "@models/Exams";
+import { TypeExams } from "@models/TypeExams";
 
 export default {
   async create(req: Request, res: Response) {
     const { type_exam_id } = req.params;
     const { description, code } = req.body;
 
-    const repo = getRepository(Exams);
+    //query verifica se o tipo do exame é válido, tratamento de erro.
+    const repoType = getRepository(TypeExams);
+    const type_exam = await repoType.findOne({
+      where: {
+        id: Number(type_exam_id),
+      },
+    });
+
+    if (!type_exam) {
+      return res.json({ message: "Por favor selecione o tipo do exame!" });
+    }
 
     try {
+      const repo = getRepository(Exams);
       await repo.save({
         description: description.toUpperCase(),
         code: code.toUpperCase(),
--- src/routes.ts
+++ src/routes.ts
@@ -6,17 +6,16 @@ import ExamsController from "@controllers/ExamsController";
 
 const routes = Router();
 
+/*Rotas para os usuarios */
 routes.post("/users", UserController.create);
 routes.get("/users", UserController.index);
 routes.put("/users/:user_id", UserController.update);
 routes.delete("/users/:user_id", UserController.delete);
 routes.get("/users/:user_id", UserController.show);
 
-//cadastra pacientes ligado a um usuario
+/*Rotas para os pacientes */
 routes.post("/users/:user_id/pacients", PacientController.create);
-//Filtra todos os pacientes que os usuario cadastrou
 routes.get("/pacients", PacientController.index);
-//Filtra apenas o paciente especifico
 routes.get("/pacients/:pacient_id", PacientController.show);
 
 /*Rotas para os tipos de exames */
